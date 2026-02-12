2c959045920cb8ed66c4ff4674795b8ee0c0e5a4
gwtr <gwtranter37@gmail.com>
2026-02-12 15:47:53 +0000

    no wrork

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,7 +1,10 @@
 setTimeout(() => {
 	let v = document.querySelector("video");
+	console.log("EExt", v);
 	v.preservesPitch = false;
 	v.playbackRate = 0.96;
 
 	console.log("EExt: set 0.96", v);
 }, 1000);
+
+console.log("contentScript loaded");
